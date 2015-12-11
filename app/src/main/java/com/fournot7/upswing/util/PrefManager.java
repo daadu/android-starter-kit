9ff84f8f62bc102ac5d1f8230e7a086632ffd05c
harsh <harsh.bhikadiya@gmail.com>
2015-12-11 15:58:42 +0530

    PrefManager created

---
+++ app/src/main/java/com/fournot7/upswing/util/PrefManager.java
@@ -0,0 +1,49 @@
+package com.fournot7.upswing.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.fournot7.upswing.R;
+
+public class PrefManager {
+
+    private String TAG = PrefManager.class.getSimpleName();
+
+    // Shared Preferences
+    SharedPreferences pref;
+    // Editor for Shared preferences
+    SharedPreferences.Editor editor;
+    // Context
+    Context context;
+    // Shared pref mode
+    final int PRIVATE_MODE = 0;
+
+    // All Shared Preferences Keys
+    private final String KEY = "key";
+
+    private SharedPreferences.OnSharedPreferenceChangeListener onSharedPreferenceChangeListener;
+
+    public PrefManager(Context context) {
+        this.context = context;
+        pref = context.getSharedPreferences(
+                context.getResources().getString(R.string.app_name),
+                PRIVATE_MODE
+        );
+
+        editor = pref.edit();
+    }
+
+    public void setOnSharedPreferenceChangeListener(SharedPreferences.OnSharedPreferenceChangeListener onSharedPreferenceChangeListener) {
+        this.onSharedPreferenceChangeListener = onSharedPreferenceChangeListener;
+        pref.registerOnSharedPreferenceChangeListener(onSharedPreferenceChangeListener);
+    }
+
+    public String getKey() {
+        return pref.getString(KEY, null);
+    }
+
+    public void setKey(String key) {
+        editor.putString(KEY, key);
+        editor.commit();
+    }
+}
