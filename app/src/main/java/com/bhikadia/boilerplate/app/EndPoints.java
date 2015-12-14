6d1cf68b098380bf50a963e6c77cca350a4e62b7
harsh <harsh.bhikadiya@gmail.com>
2015-12-15 02:17:17 +0530

    minor

--- app/src/main/java/com/bhikadia/boilerplate/app/EndPoints.java
+++ app/src/main/java/com/bhikadia/boilerplate/app/EndPoints.java
@@ -4,8 +4,8 @@ package com.bhikadia.boilerplate.app;
  * Created by harsh on 11/12/15.
  */
 public class EndPoints {
-    private static String BASE_URL = "http://daadu.github.io/example/";
+    private static final String BASE_URL = "http://daadu.github.io/example/";
 
-    public static String REGISTER_API = BASE_URL + "register.json";
-    public static String LIST_API = BASE_URL + "list.json";
+    public static final String REGISTER_API = BASE_URL + "register.json";
+    public static final String LIST_API = BASE_URL + "list.json";
 }
